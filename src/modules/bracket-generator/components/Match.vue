17ce82b85d2835c84fa070cbf7e573281aaa11ba
Stefan van de Griendt <svdgriendt@users.noreply.github.com>
2022-08-19 18:30:37 +0200

    Fix re-rendering problem with nested `Match` components
    Only when Vue3 decided to completely re-render the `Match` component,
    any added child `Match` components would be rendered. But because that's
    not always the case, there was an issue, in particular when a `Match`
    went from two direct values, to having a child `Match`.
    
    The cause is that `hasMatchModel` was stored only when the `Match`
    component was rendered, but the child `Match` components are dependent
    on it. So now changed it to a `computed`, which tells Vue3 to properly
    render the child `Match`.

--- src/modules/bracket-generator/components/Match.vue
+++ src/modules/bracket-generator/components/Match.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import MatchModel from '../match';
 
 const props = defineProps({ match: { type: MatchModel, required: true } });
-const hasMatchModel = MatchModel.is(props.match.top) || MatchModel.is(props.match.bottom);
+const hasMatchModel = computed(() => {
+  return MatchModel.is(props.match.top) || MatchModel.is(props.match.bottom);
+});
 </script>
 
 <template>
@@ -12,7 +15,8 @@ const hasMatchModel = MatchModel.is(props.match.top) || MatchModel.is(props.matc
       <div class="contestant" data-position="bottom">{{ !MatchModel.is(match.bottom) ? match.bottom : '&nbsp;' }}</div>
     </div>
 
-    <div class="matches" :class="{ top: MatchModel.is(match.top), bottom: MatchModel.is(match.bottom) }" v-if="hasMatchModel">
+    <div class="matches" :class="{ top: MatchModel.is(match.top), bottom: MatchModel.is(match.bottom) }"
+      v-if="hasMatchModel">
       <Match data-position="top" v-if="MatchModel.is(match.top)" :match="match.top" />
       <Match data-position="bottom" v-if="MatchModel.is(match.bottom)" :match="match.bottom" />
 
