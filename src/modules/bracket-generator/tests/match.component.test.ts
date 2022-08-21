6168953e7501f380924b6ed72b519b2d0baa0165
Stefan van de Griendt <svdgriendt@users.noreply.github.com>
2022-08-22 00:33:22 +0200

    Add nested `Match` component tests
    Instead of querying through multiple layers of a component, instead the
    single component test for that component should test those cases. So,
    now we instead check if a specific component type is rendered with the
    expected data in it.

--- src/modules/bracket-generator/tests/match.component.test.ts
+++ src/modules/bracket-generator/tests/match.component.test.ts
@@ -19,8 +19,50 @@ describe('Single Match component rendering', () => {
   });
 });
 
-describe.todo('Nested Match component rendering (waiting for https://github.com/capricorn86/happy-dom/issues/564)', () => {
-  test.todo('Match = (1,(2,3))');
-  test.todo('Match = ((1,4),(2,3))');
-  test.todo('Match = ((1,(4,5)),(2,3))');
+describe('Nested Match component rendering', () => {
+  test('Match = (1,(2,3))', () => {
+    const wrapper = mount(MatchComponent, { props: { match: new Match(1, new Match(2, 3)) } });
+    expect(wrapper.find('.contestants .contestant[data-position="top"]').text()).toEqual('1');
+    expect(wrapper.find('.contestants .contestant[data-position="bottom"]').text()).toEqual('');
+
+    const matches = wrapper.find('.matches');
+    expect(matches.classes()).toEqual(['matches', 'bottom']);
+
+    expect(() => matches.getComponent<typeof MatchComponent>('.match[data-position="top"]')).toThrowError();
+
+    const bottom = matches.getComponent<typeof MatchComponent>('.match[data-position="bottom"]');
+    expect(bottom.vm.$options.__name).toEqual(MatchComponent.__name);
+    expect(bottom.vm.match).toEqual(new Match(2, 3));
+  });
+  test('Match = ((1,3),2)', () => {
+    const wrapper = mount(MatchComponent, { props: { match: new Match(new Match(1, 3), 2) } });
+    expect(wrapper.find('.contestants .contestant[data-position="top"]').text()).toEqual('');
+    expect(wrapper.find('.contestants .contestant[data-position="bottom"]').text()).toEqual('2');
+
+    const matches = wrapper.find('.matches');
+    expect(matches.classes()).toEqual(['matches', 'top']);
+
+    const top = matches.getComponent<typeof MatchComponent>('.match[data-position="top"]');
+    expect(top.vm.$options.__name).toEqual(MatchComponent.__name);
+    expect(top.vm.match).toEqual(new Match(1, 3));
+
+    expect(() => matches.getComponent<typeof MatchComponent>('.match[data-position="bottom"]')).toThrowError();
+
+  });
+  test('Match = ((1,4),(2,3))', () => {
+    const wrapper = mount(MatchComponent, { props: { match: new Match(new Match(1, 4), new Match(2, 3)) } });
+    expect(wrapper.find('.contestants .contestant[data-position="top"]').text()).toEqual('');
+    expect(wrapper.find('.contestants .contestant[data-position="bottom"]').text()).toEqual('');
+
+    const matches = wrapper.find('.matches');
+    expect(matches.classes()).toEqual(['matches', 'top', 'bottom']);
+
+    const top = matches.getComponent<typeof MatchComponent>('.match[data-position="top"]');
+    expect(top.vm.$options.__name).toEqual(MatchComponent.__name);
+    expect(top.vm.match).toEqual(new Match(1, 4));
+
+    const bottom = matches.getComponent<typeof MatchComponent>('.match[data-position="bottom"]');
+    expect(bottom.vm.$options.__name).toEqual(MatchComponent.__name);
+    expect(bottom.vm.match).toEqual(new Match(2, 3));
+  });
 });
